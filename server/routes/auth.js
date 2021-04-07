511405a68abb1c9e1e18d3eff929a08b2fe2eba8
FinalHexcor <58568739+FinalHexcor@users.noreply.github.com>
2021-04-07 18:54:54 +0100

    vehicule table

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,109 +1,109 @@
-const router = require('express').Router();
-const User = require('../model/User');
-const Mission = require('../model/Mission');
-const bcrypt = require ('bcryptjs');
-const jwt = require('jsonwebtoken');
+const router = require("express").Router();
+const User = require("../model/User");
+const Mission = require("../model/Mission");
+const Vehicule = require("../model/Vehicule");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-const cors = require('cors')
-router.use(cors())
+const cors = require("cors");
+router.use(cors());
 //list users
-router.get('/list',async (req,res) => {
-   
-     const resultat = await User.find()
-     
-    return res.send(resultat);
-    
-    });
-// register user
-router.post('/register',async (req,res)=>{
+router.get("/list", async (req, res) => {
+  const resultat = await User.find();
 
- const mailexist = await User.findOne({mail: req.body.mail})
- if(mailexist) return res.status(200).send('mail exists !!');
-const salt = await bcrypt.genSalt(10);
-const hashPassword = await bcrypt.hash(req.body.password, salt);
-
- const user = new User({
-     name: req.body.name,
-     password :hashPassword,
-     mail : req.body.mail,
- });
-try{
-const savedUser = await user.save();
-res.send('user created');
-
-}catch(err){
-    res.status(400).send(err);
-}
-
+  return res.send(resultat);
 });
+//list vehicules
+router.get("/listV", async (req, res) => {
+  const resultat = await Vehicule.find();
 
+  return res.send(resultat);
+});
+// register user
+router.post("/register", async (req, res) => {
+  const mailexist = await User.findOne({ mail: req.body.mail });
+  if (mailexist) return res.status(200).send("mail exists !!");
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(req.body.password, salt);
+
+  const user = new User({
+    name: req.body.name,
+    password: hashPassword,
+    mail: req.body.mail,
+  });
+  try {
+    const savedUser = await user.save();
+    res.send("user created");
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
 
 // Add Mission
-router.post('/addMission',async (req,res)=>{
-    const mission = new Mission ({
-        missionId : req.body.id,
-        status: req.body.status,
-        contact : req.body.contact,
-        priority : req.body.priority,
-        comment : req.body.comment,
-       
-        
-    });
-   
-   try{
-   const savedMission = await mission.save();
-   res.send('mission created');
-   
-   }catch(err){
-       res.status(400).send(err);
-   }
-   
-   });
+router.post("/addMission", async (req, res) => {
+  const mission = new Mission({
+    missionId: req.body.id,
+    status: req.body.status,
+    contact: req.body.contact,
+    priority: req.body.priority,
+    comment: req.body.comment,
+  });
 
+  try {
+    const savedMission = await mission.save();
+    res.send("mission created");
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+// Add Vehicule
+router.post("/addVehicule", async (req, res) => {
+  const vehiculeexist = await Vehicule.findOne({
+    matricule: req.body.matricule,
+  });
+  if (vehiculeexist) return res.status(200).send("Vehicule exists !!");
+  const vehicule = new Vehicule({
+    matricule: req.body.matricule,
+    brand: req.body.brand,
+    year: req.body.year,
+  });
 
+  try {
+    const savedVehicule = await vehicule.save();
+    res.send("Vehicule added");
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+//get name
+router.post("/data", async (req, res) => {
+  const user = await User.findOne({ mail: req.body.mail });
 
-
-
-
-
-
-//get name 
-router.post('/data', async (req,res) => {
-
-    const user = await User.findOne({mail: req.body.mail})
-    
- 
-   res.send(user);
-    
-
-    
-    });
-
-
-
-
-//login
-router.post('/login', async (req,res) => {
-
-const user = await User.findOne({mail: req.body.mail})
-if(!user) return res.status(200).send('mail doesnt exist');
-  
-
-const validPass = await bcrypt.compare(req.body.password, user.password);
-if(!validPass) return res.send('invalid password');
-if(validPass) return res.send('Connected');
-
-// login token
-
-/* const token = jwt.sign({id: user._id}, process.env.TOKEN_SECRET );
-res.header('auth-token',token).send(token); */
-
-
-
-
-
+  res.send(user);
 });
 
+// delete user
+router.post("/delete", async (req, res) => {
+  const user = await User.deleteOne({ mail: req.body.mail });
 
+  res.send("deleted");
+});
 
-module.exports = router ;
+//login
+router.post("/login", async (req, res) => {
+  const user = await User.findOne({ mail: req.body.mail });
+  if (!user) return res.status(200).send("mail doesnt exist");
+
+  const validPass = await bcrypt.compare(req.body.password, user.password);
+  if (!validPass) return res.send("invalid password");
+  if (validPass) return res.send("Connected");
+
+  // login token
+
+  /* const token = jwt.sign({id: user._id}, process.env.TOKEN_SECRET );
+res.header('auth-token',token).send(token); */
+
+  // delete user
+});
+
+module.exports = router;
